4f380f4f651c49afb94fc9a8dd6a31f390393536
Guilherme Ianino <guilherme.ianino@gmail.com>
2024-10-11 14:51:11 -0300

    Configuração Bson UUID

--- aspnet-core/src/ecommerce.Domain/Data-seeder/ProductDataSeeder.cs
+++ aspnet-core/src/ecommerce.Domain/Data-seeder/ProductDataSeeder.cs
@@ -1,47 +1,47 @@
-using System;
-using System.Threading.Tasks;
-using ecommerce.Products;
-using Volo.Abp.Data;
-using Volo.Abp.DependencyInjection;
-using Volo.Abp.Domain.Repositories;
+// using System;
+// using System.Threading.Tasks;
+// using ecommerce.Products;
+// using Volo.Abp.Data;
+// using Volo.Abp.DependencyInjection;
+// using Volo.Abp.Domain.Repositories;
 
-namespace ecommerce;
+// namespace ecommerce;
 
-public class ProductStoreDataSeederContributor
-    : IDataSeedContributor, ITransientDependency
-{
-    private readonly IRepository<Product, Guid> _ProductRepository;
+// public class ProductStoreDataSeederContributor
+//     : IDataSeedContributor, ITransientDependency
+// {
+//     private readonly IRepository<Product, Guid> _ProductRepository;
 
-    public ProductStoreDataSeederContributor(IRepository<Product, Guid> ProductRepository)
-    {
-        _ProductRepository = ProductRepository;
-    }
+//     public ProductStoreDataSeederContributor(IRepository<Product, Guid> ProductRepository)
+//     {
+//         _ProductRepository = ProductRepository;
+//     }
 
-    public async Task SeedAsync(DataSeedContext context)
-    {
-        if (await _ProductRepository.GetCountAsync() <= 0)
-        {
-            await _ProductRepository.InsertAsync(
-                new Product
-                {
-                    Name = "Moletom Supreme",
-                    Description = "Roupa de frio estilo moletom",
-                    Price = 99.99,
-                    Category = ProductCategory.Clothes
-                },
-                autoSave: true
-            );
+//     public async Task SeedAsync(DataSeedContext context)
+//     {
+//         if (await _ProductRepository.GetCountAsync() <= 0)
+//         {
+//             await _ProductRepository.InsertAsync(
+//                 new Product
+//                 {
+//                     Name = "Moletom Supreme",
+//                     Description = "Roupa de frio estilo moletom",
+//                     Price = 99.99,
+//                     Category = ProductCategory.Clothes
+//                 },
+//                 autoSave: true
+//             );
 
-            await _ProductRepository.InsertAsync(
-                new Product
-                {
-                    Name = "Moletom Nike",
-                    Description = "Roupa de frio estilo moletom",
-                    Price = 99.99,
-                    Category = ProductCategory.Clothes
-                },
-                autoSave: true
-            );
-        }
-    }
-}
+//             await _ProductRepository.InsertAsync(
+//                 new Product
+//                 {
+//                     Name = "Moletom Nike",
+//                     Description = "Roupa de frio estilo moletom",
+//                     Price = 99.99,
+//                     Category = ProductCategory.Clothes
+//                 },
+//                 autoSave: true
+//             );
+//         }
+//     }
+// }

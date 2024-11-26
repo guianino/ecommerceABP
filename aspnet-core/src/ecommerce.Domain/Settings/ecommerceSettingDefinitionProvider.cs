574cf03128e9c9c1cac89f94f5503285771f9e42
Guilherme Ianino <guilherme.ianino@gmail.com>
2024-11-26 20:41:01 -0300

    Exemplo de configuração de parametros

--- aspnet-core/src/ecommerce.Domain/Settings/ecommerceSettingDefinitionProvider.cs
+++ aspnet-core/src/ecommerce.Domain/Settings/ecommerceSettingDefinitionProvider.cs
@@ -8,5 +8,37 @@ public class ecommerceSettingDefinitionProvider : SettingDefinitionProvider
     {
         //Define your own settings here. Example:
         //context.Add(new SettingDefinition(ecommerceSettings.MySetting1));
+        context.Add(
+            new SettingDefinition(ecommerceSettings.AllowPaymentWithPix)
+        );
     }
 }
+
+
+
+// public class PaymentService
+// {
+//     private readonly ISettingProvider _settingProvider;
+
+//     public PaymentService(ISettingProvider settingProvider)
+//     {
+//         _settingProvider = settingProvider;
+//     }
+
+//     public async Task<bool> IsPixEnabledAsync()
+//     {
+//         return await _settingProvider.GetAsync<bool>(ecommerceSettings.AllowPaymentWithPix);
+//     }
+
+//     public async Task ProcessPaymentAsync(string paymentMethod)
+//     {
+//         if (paymentMethod == "Pix")
+//         {
+//             if (!await IsPixEnabledAsync())
+//             {
+//                 throw new Exception("Payment with Pix is currently disabled.");
+//             }
+
+//         }
+//     }
+// }
--- aspnet-core/src/ecommerce.Domain/Settings/ecommerceSettings.cs
+++ aspnet-core/src/ecommerce.Domain/Settings/ecommerceSettings.cs
@@ -2,8 +2,6 @@
 
 public static class ecommerceSettings
 {
-    private const string Prefix = "ecommerce";
-
-    //Add your own setting names here. Example:
-    //public const string MySetting1 = Prefix + ".MySetting1";
+    private const string GroupName = "ecommerce";
+    public const string AllowPaymentWithPix = $"{GroupName}.AllowPaymentWithPix";
 }
